0160efb643629c3a58060b2fa15845c7d7b8c51c
wakanyan <kyoto.kobe.1126.yuto@gmail.com>
2019-10-27 01:37:16 +0900

    Initial Commit

---
+++ Gacha2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gacha2
+//
+//  Created by Yuto Wakabayashi on 2019/10/24.
+//  Copyright © 2019 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
